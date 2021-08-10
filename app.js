958c0c23fcf97447423a6c37b9e46c54695768bd
ojun-young <junyoung.oh@gooroomee.com>
2021-08-10 21:34:56 +0900

    메소드 랩핑 형식으로 개발 중

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
-let comNumber = [];
-let userNumber = [];
+let BBGame = function () {
 
-//서로 다른 4자리 랜덤 수 생성
-let createRandomNum = () => {
+}
+
+// 서로 다른 4자리 랜덤 수 생성
+BBGame.getComAnswer = () => {
     const numList = [0,1,2,3,4,5,6,7,8,9]
     let randomNum = []
     for (let i = 0; i < 4; i++) {
         let pick = Math.floor(Math.random() * numList.length)
         randomNum[i] = numList.splice(pick, 1)[0]
     }
-    comNumber.push(randomNum)
+    return randomNum
 }
 
-// 입력받은 수 배열 변환
-function inputNumber(num) {
-    const inputArray = num.toString().split('')
-    userNumber.push(inputArray)
+// 입력 받은 수 배열 변환
+BBGame.getUserAnswer = (enterNum) => {
+    const array = [];
+    const strArray = enterNum.toString().split('')
+    for (let i = 0; i < strArray.length; i++) {
+        array.push(Number(strArray[i]))
+    }
+    return array
 }
 
-// 판정
-function decisionNumber() {
+// 컴퓨터 랜덤 수와 입력한 수 비교
+BBGame.compareAnswers = (comAnswer, userAnswer) => {
     let result = {
         strike : 0,
         ball : 0,
-        out : 0,
-        count : 0
+        homeRun : 0
     }
-    if (comNumber.equals())
 
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (comNumber[i] == userNumber[j]) {
-                if (i === j) {
-                    result.strike++;
-                } else {
-                    result.ball++
+    if (JSON.stringify(comAnswer) === JSON.stringify(userAnswer)) {
+        result.homeRun++
+        return result;
+    } else {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (comAnswer[i] == userAnswer[j]) {
+                    if (i === j) {
+                        result.strike++;
+                    } else {
+                        result.ball++
+                    }
                 }
-                break;
             }
         }
+        return result
     }
-    result.count++
-    if (result.strike === 4) {
-        console.log()
-    }
-    
 }
 
 
