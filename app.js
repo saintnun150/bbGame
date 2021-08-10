46b5f68779cedc2668d158dfba965b92d8b88177
ojun-young <junyoung.oh@gooroomee.com>
2021-08-10 22:04:43 +0900

    startGame 추가

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ let BBGame = function () {
 
 }
 
+BBGame.startGame = () => {
+    let ca = BBGame.getComAnswer()
+    let typing = prompt('숫자를 입력하세요')
+    let ua = BBGame.getUserAnswer(typing)
+    return BBGame.compareAnswers(ca, ua)
+
+}
+
 // 서로 다른 4자리 랜덤 수 생성
 BBGame.getComAnswer = () => {
     const numList = [0,1,2,3,4,5,6,7,8,9]
@@ -33,13 +41,13 @@ BBGame.compareAnswers = (comAnswer, userAnswer) => {
 
     if (JSON.stringify(comAnswer) === JSON.stringify(userAnswer)) {
         result.homeRun++
-        return result;
+        return result
     } else {
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
                 if (comAnswer[i] == userAnswer[j]) {
                     if (i === j) {
-                        result.strike++;
+                        result.strike++
                     } else {
                         result.ball++
                     }
